93c62316462812b188c5c573e7222b32db5d7613
Adam Evans <adam3van5@gmail.com>
2024-09-05 22:30:04 +0900

    alternate syntax

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,9 +1,11 @@
-import { type ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
+// import { type ReactNode } from 'react';
 
-interface CourseGoalProps {
-  title: string;
-  children: ReactNode;
-}
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+type CourseGoalProps = PropsWithChildren<{ title: string }>;
 export default function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
